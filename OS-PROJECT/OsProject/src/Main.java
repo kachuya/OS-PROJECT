0f16ac72c0d78d3972d48373cdf1ff2a372e9e78
lenah hamed <foom.latatata@gmail.com>
2024-11-05 00:39:51 +0300

    test

--- OS-PROJECT/OsProject/src/Main.java
+++ OS-PROJECT/OsProject/src/Main.java
@@ -8,49 +8,38 @@ import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
-    static int NC, NW, NF, NT, NOM, NCM; //customer line, waiters, chefs, tables, ordered meals, cooked meals
+    static int NF, NW, NT; //customer line, waiters, chefs, tables, ordered meals, cooked meals
     static Map<String, Integer> mealTimes = new HashMap<>(); // hashmap to store meal times , key = meal name(string)
                                                              // and int = time to prepare
-    static List<Customer> customers = new ArrayList<>(NC); // list to store customer objects
-    //static Semaphore table;     // available tables
-    //static Semaphore chef;     // available chefs
-    //static Semaphore waiter;  // available waiters
-    //static Queue<Order> orderQueue = new ConcurrentLinkedQueue<>(); // thread-safe queue to store orders
-    //static List<Meal> mealsReady = Collections.synchronizedList(new ArrayList<>()); // thread-safe list to store cooked
-                                                                                    // meals
+    static Queue<Customer> customerKiosk = new ConcurrentLinkedQueue<>(); // list to store customer objects
 
-    static BlockingQueue tables = new ArrayBlockingQueue<>(NT);
-    static BlockingQueue orderedMeal = new ArrayBlockingQueue<>(NOM);
-    static BlockingQueue cookedMeal = new ArrayBlockingQueue<>(NCM);
+    static Semaphore customer; 
+    static Semaphore table;    
+    static Semaphore chef;     
+    static Semaphore waiter;
+    static int mutex;
+    
+    static Queue<String> orderQueue = new ConcurrentLinkedQueue<>(); // thread-safe queue to store orders
+    static List<String> cookedMeal = Collections.synchronizedList(new ArrayList<>()); // thread-safe list to store cooked
+    // meals
 
     static AtomicInteger custServed = new AtomicInteger(0); // to count number of customers served
 
     public static void main(String[] args) throws IOException {
 
+        Random r = new Random(); // to simulate delays
+
         String Rfile = "C:\\Users\\lenah\\Desktop\\OS-PROJECT\\OS-PROJECT\\input files\\restaurant_simulation_input1.txt";
         inputConfig(Rfile);
 
-       // for (int i = 0; i < NF; i++) {
-       //    Chef chef = new Chef(i);
-       //    Thread cthread = new Thread(chef);
-       //   cthread.start();
-       //}
+        long startTime = System.currentTimeMillis();
 
-        //for (int i = 0; i < NW; i++) {
-        //    Waiter waiter = new Waiter(i);
-        //    Thread wthread = new Thread(waiter);
-        //   wthread.start();
-        //}
+        customer = new Semaphore(customerKiosk.size());
+        table = new Semaphore(NT);
+        chef = new Semaphore(NF);
+        waiter = new Semaphore(NW);
 
-        System.out.print("Simulation Started with " + NF + " Chefs, " + NW + " Waiters, and " + NT + " Tables.");
-
-        //table = new Semaphore(NT, true);
-        //chef = new Semaphore(NF, true);
-        //waiter = new Semaphore(NW, true);
-
-        for (Customer customer : customers) {
-            new Thread(customer).start();
-        }
+        System.out.print("Simulation Started with " + NF + " Chefs, " + NW + " Waiters, and " + NT + " Tables.");        
 
     }
 
@@ -63,17 +52,9 @@ public class Main {
 
         String line1 = br.readLine();
         String[] fp = line1.split(" "); // Split by space
-        NC = Integer.parseInt(fp[0].split("=")[1]); // customer line size
+        NF = Integer.parseInt(fp[0].split("=")[1]); // customer line size
         NW = Integer.parseInt(fp[1].split("=")[1]);
-        NF = Integer.parseInt(fp[2].split("=")[1]);
-
-        String line2 = br.readLine();
-        String[] sp = line2.split(" "); // Split by space
-        NT = Integer.parseInt(sp[0].split("=")[1]); // table size
-        NOM = Integer.parseInt(sp[1].split("=")[1]); // ordered meal size
-        NCM = Integer.parseInt(sp[2].split("=")[1]); // cooked meal size
-
-        //System.out.print("Simulation Started with " + NF + " Chefs, " + NW + " Waiters, and " + NT + " Tables.");
+        NT = Integer.parseInt(fp[2].split("=")[1]);
 
         String mealLine = br.readLine();
         String[] meals = mealLine.split(" ");
@@ -82,15 +63,23 @@ public class Main {
             String mealName = parts[0];
             int prepTime = Integer.parseInt(parts[1].split(":")[1]);
             mealTimes.put(mealName, prepTime);
-
         }
+
         // reading the remaining lines to store customer info and adding it to customer
         // list
 
         String remainingline;
+
         while ((remainingline = br.readLine()) != null) {
-            customers.add(new Customer(remainingline));
+            Customer c = new Customer();
+            String[] parts = remainingline.split(" ");
+
+            c.setId(Integer.parseInt(parts[0].split("=")[1]));
+            c.setTime(parts[1].split("=")[1]);
+            c.setOrder(parts[2].split("=")[1]);
+            customerKiosk.add(c);
         }
+
         br.close(); // Close the BufferedReader
     }
 
@@ -100,52 +89,89 @@ class Restaraunt {
     
 }
 
-class Customer implements Runnable {
+class Customer extends Main implements Runnable { 
     int custid;
-    String arrivalTime;
+    int arrivalTime;
     String order;                            
-    private final int orderPrepTime;
+    int orderPrepTime;
 
 
-    public Customer(int id, String meal) { 
-        
-        
-        
-   
+    // public Customer() {
 
-        
+    // }
+    
+    public void setId(int i) {
+        this.custid = i;
+    }
+    
+    public void setTime(String t) {
+        int t1 = Integer.parseInt(t.split(":")[1]) - 8;
+        this.arrivalTime = t1;
+    }
+    
+    public void setOrder(String o) {
+        this.order = o;
+        this.orderPrepTime = mealTimes.get(order);
     }
 
     @Override
     public void run() {
+        try {
+            
+        }
+        catch (InterruptedException ex) {
 
-    }
-
-    public void getSeated() { // produce -- customer is seated at a table
+        }
         
+        // int m = Integer.parseInt(arrivalTime.split(":")[1]) - 8;
+        // TimeUnit.MINUTES.sleep(m);
+
+        //     System.out.printf("%n[%s] Customer %d arrives.%n", arrivalTime, custid);
+            
+
+        //     // Place the order
+        //     synchronized (orderQueue) {
+        //         orderQueue.add(new Order(custid, order));
+        //         System.out.printf("%n[%s] Customer %d places an order: %s.%n", arrivalTime, id, order);
+
+        //         //wait for  order
+        //         TimeUnit.MINUTES.sleep(orderPrepTime);
+        //         System.out.printf("[%s] Customer %d finishes eating and leaves the restaurant.%n", arrivalTime, id);
+
+                // Release the table
+                // tables.remove();
     }
 }
+
+    // public void getSeated() { // produce -- customer is seated at a table
+               
+    // }
 
 class Chef implements Runnable {
 
     int chefid;
 
     public Chef(int i) {
-        
+        this.chefid = i;
     }
 
     @Override
     public void run() {
-
+        try {
+            
+        }
+        catch (InterruptedException ex) {
+            
+        }
     }
 
-    public void cookMeal() { // consume -- chef begins preparing a customer's ordered meal from orderedMeal
+    // public void cookMeal() { // consume -- chef begins preparing a customer's ordered meal from orderedMeal
 
-    }
+    // }
     
-    public void hands() { // produce -- produce a meal in cookedMeal
+    // public void hands() { // produce -- produce a meal in cookedMeal
 
-    }
+    // }
 }
 
 class Waiter implements Runnable {
@@ -153,20 +179,23 @@ class Waiter implements Runnable {
     int waiterid;
 
     public Waiter(int i) {
-        
+
     }
 
     @Override
     public void run() {
+        try {
 
+        } catch (InterruptedException ex) {
+
+        }
     }
 
-    public void removeSeat() { // consume -- waiter releases a table after customer has left
+    // public void removeSeat() { // consume -- waiter releases a table after customer has left
 
-    }
-    
-    public void serveMeal() { // consume -- waiter serves a meal to customer from cookedMeal
-        
-    }
+    // }
+
+    // public void serveMeal() { // consume -- waiter serves a meal to customer from cookedMeal
+
+    // }
 }
-
